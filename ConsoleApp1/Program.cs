f2f88fcae9b14fb0bacdf9df47c89376e6577b90
Basithkhan Ibrahim <Basithkhan.Ibrahim@gds.ey.com>
2020-06-20 14:45:47 +0530

    commited for merge

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("It's a beginning!");
+            Console.WriteLine("bye world");
         }
     }
 }
